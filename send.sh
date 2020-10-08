8a64508bb4d523c377772a3e4d89def10e8887fe
Kieran Bingham <kieran@bingham.xyz>
2020-10-08 14:38:22 +0100

    Initial debug for a JSON get/post storage

---
+++ send.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -d @data.json http://127.0.0.1:5000/
---
+++ targets.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('targets', type=list, location='json')
+
+
+TARGETS = {
+        "targets": [ 1, 2, 3 ] 
+}
+
+class Targets(Resource):
+    def get(self):
+        print("Getting")
+        print(TARGETS)
+        return TARGETS
+
+    def post(self):
+        request.get_json(force=True)
+        args = parser.parse_args()
+
+        TARGETS = args.copy()
+
+        print("Args")
+        print(args)
+        print("TARGETS")
+        print(TARGETS)
+
+        return TARGETS
+
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(Targets, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
